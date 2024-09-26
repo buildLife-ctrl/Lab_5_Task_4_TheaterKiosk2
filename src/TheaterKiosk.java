b33fc0151572e53383643b8994261c5d287a8a98
617998 <sinchuri.617998@swl.k12.oh.us>
2024-09-26 09:37:03 -0400

    4

--- src/TheaterKiosk.java
+++ src/TheaterKiosk.java
@@ -1,5 +1,25 @@
-public class Main {
+import java.util.Scanner;
+
+public class TheaterKiosk {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        //variable
+        int userAge;
+        int MIN_AGE = 21;
+
+        //asks the user their age
+        System.out.println("What is your age?");
+        //checks and outputs the correct response
+        if (scan.hasNextInt()) {
+            userAge = scan.nextInt();
+            if (userAge >= MIN_AGE) {
+                System.out.println("You get a wristband!");
+            } else {
+                System.exit(0);
+            }
+        } else {
+            System.out.println("You have entered an invalid data type.");
+            System.exit(0);
+        }
     }
 }
